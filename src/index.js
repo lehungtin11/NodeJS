5614029349b851119f165861f6b90ae450925d92
lehungtin11 <hungtin640@gmail.com>
2022-10-08 20:18:21 +0700

    PORT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT ||3000;
 const http = require('http');
 const server = http.createServer(app);
 const session = require('express-session');
