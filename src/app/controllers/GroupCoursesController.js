c2a8847da217727804dcd2d657aec165ed744362
lehungtin11 <hungtin640@gmail.com>
2022-11-24 16:40:57 +0700

    Complete group courses, chat box...

--- src/app/controllers/GroupCoursesController.js
+++ src/app/controllers/GroupCoursesController.js
@@ -47,6 +47,70 @@ class GroupCoursesController {
 
     // [PUT] /group-courses/update
 
+    // [DELETE] /soft-delete/:id
+    softDelete(req, res, next) {
+        MyGroupCourses.delete({_id: req.params.id})
+        .then(res.redirect('/group-courses'))
+        .catch(next)
+    }
+
+    // [GET] /trash-bin
+    trashBin(req, res, next) {
+        let page = parseInt(req.query.page) || 1;
+        let perPage = 5;
+        let start = (page - 1) * perPage;
+        let end = (page - 1) * perPage + perPage;
+        Promise.all([MyGroupCourses.findDeleted(),MyGroupCourses.countDeleted()])
+        .then(([data, total]) => {
+            let totalPage = Math.ceil(total/perPage);
+            if(total <= 0 || page >= 0 && page <= totalPage ) {
+                total <= 0 ? page = 1 :'' ;
+                res.render('groupCourses/trash', {
+                    data: ToObject.manyData(data.slice(start,end)),
+                    username: req.session.username,
+                    totalPage: totalPage,
+                    currentPage: page,
+                })
+            } else {
+                res.render('notFound')
+            }
+        })        
+        .catch(next)
+    }
+
+    // [DELETE] /group-courses/trash-bin/:id
+    destroy(req, res, next) {
+        MyGroupCourses.deleteOne({_id: req.params.id})
+        .then(res.redirect('/group-courses/trash-bin'))
+        .catch(next)
+    }
+
+    // [GET] /restore
+    restore(req, res, next) {
+        MyGroupCourses.restore({name: req.query.slug})
+        .then(res.redirect('back'))
+        .catch(next)
+    }
+
+    // [GET] /update
+    update(req, res, next) {
+            MyGroupCourses.findOne({_id: req.query.slug})
+        .then((data) => {
+            res.render('groupCourses/update',{
+                data: ToObject.oneData(data),
+            })
+        })
+        .catch(next)
+    }
+    
+    // [GET] /updated
+    updated(req, res, next) {
+        req.body.slug = req.body.thumbnail;
+        MyGroupCourses.updateOneWithDeleted({_id: req.query.slug}, req.body, function(err) {
+            if(err) return console.log(err)
+        })
+        res.redirect('/group-courses')
+    }
 }
 
 module.exports = new GroupCoursesController
--- src/routes/groupCourses.js
+++ src/routes/groupCourses.js
@@ -4,6 +4,12 @@ const middleware = require('../middleware')
 const GroupCoursesController = require('../app/controllers/GroupCoursesController')
 
 router.get('/', GroupCoursesController.index)
+router.delete('/soft-delete/:id', GroupCoursesController.softDelete)
+router.delete('/trash-bin/:id', GroupCoursesController.destroy)
+router.get('/update', GroupCoursesController.update)
+router.put('/updated', GroupCoursesController.updated)
+router.get('/trash-bin', GroupCoursesController.trashBin)
+router.get('/restore', GroupCoursesController.restore)
 router.get('/create', GroupCoursesController.create)
 router.post('/create', GroupCoursesController.create)
 
